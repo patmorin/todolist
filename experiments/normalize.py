bc21444b16ffbc36cb1f40ea69a9e3c92727dd5d
Pat Morin <morin@scs.carleton.ca>
2014-07-03 16:58:27 -0400

    Starting to shape up nicely.

---
+++ experiments/normalize.py
@@ -0,0 +1,33 @@
+from __future__ import division
+
+structs={"todolist-0.2", "todolist-0.35", "skiplist", "redblack", "treap",  
+         "scapegoat", "bst", "sortedarray"}
+
+ns = range(25000, 2000001, 25000)
+print 'Reading bst-find.dat'
+data = [s.split() for s in open('bst-find.dat').read().splitlines()]
+denominators = dict([ (x[2], x[3]) for x in data])
+for s in structs:
+    infile = '{}-find.dat'.format(s)
+    outfile = '{}-find-norm.dat'.format(s)
+    data = [s.split() for s in open(infile).read().splitlines()]
+    print "Normalizing {} saving to {}".format(infile, outfile)
+    of = open(outfile, 'w')
+    for d in data:
+        denom = denominators[d[2]]
+        of.write("{} {}\n".format(d[2], float(d[3])/float(denom)))
+        
+structs.remove('bst')
+structs.remove('sortedarray')
+data = [s.split() for s in open('redblack-add.dat').read().splitlines()]
+denominators = dict([ (x[2], x[3]) for x in data])
+for s in structs:
+    infile = '{}-add.dat'.format(s)
+    outfile = '{}-add-norm.dat'.format(s)
+    data = [s.split() for s in open(infile).read().splitlines()]
+    print "Normalizing {} saving to {}".format(infile, outfile)
+    of = open(outfile, 'w')
+    for d in data:
+        denom = denominators[d[2]]
+        of.write("{} {}\n".format(d[2], float(d[3])/float(denom)))
+ 
